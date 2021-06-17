349c2e42298e5ac408cd47bd3309fcadc04907da
papilong123 <2502643682@qq.com>
2021-06-17 10:18:35 +0800

    添加list_sys_status.sh和mysqlbak.sh

---
+++ list_sys_status.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# 显示系统使用的以下信息：
+# 主机名、IP地址、子网掩码、网关、DNS服务器IP地址信息
+IP=`ifconfig eth0 | head -2 | tail -1 | awk '{print $2}' | awk -F":" '{print $2}'`
+ZW=` ifconfig eth0 | head -2 | tail -1 | awk '{print $3}' | awk -F":" '{print $2}'`
+GW=`route -n | tail -1 | awk '{print $2}'`
+HN=`hostname`
+DNS=`head -1 /etc/resolv.conf | awk '{print $2}'`
+echo '此机IP地址是' $IP
+echo '此机子网掩码是' $ZW
+echo '此机网关是' $GW
+echo '此机主机名是' $HN
+echo '此机DNS是' $DNS
---
+++ mysqlbak.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 备份数据库目录脚本
+DAY=`date +%Y%m%d`
+SIZE=`du -sh /var/lib/mysql`
+echo "Date: $DAY" >> /tmp/dbinfo.txt
+echo "Data Size: $SIZE" >> /tmp/dbinfo.txt
+cd /opt/dbbak &> /dev/null || mkdir /opt/dbbak
+tar zcf /opt/dbbak/mysqlbak-${DAY}.tar.gz /var/lib/mysql /tmp/dbinfo.txt &> /dev/null
+rm -f /tmp/dbinfo.txt
+
+crontab-e
+55 23 */3 * * /opt/dbbak/dbbak.sh
